f381eb5bd5a00c06b77b1e72f3667194207d23c9
Fisraiagan <Francis-hagan@live.ca>
2026-05-13 15:37:02 -0400

    initilizing DOM variables

---
+++ Lab1/js/index.js
@@ -0,0 +1,10 @@
+ characterName = document.querySelector(".CharacterName");
+age = document.querySelector(".age");
+isSuperhero = document.querySelector(".isSuperhero");
+specialpowers = document.querySelector(".specialpowers");
+favoriteFood = document.querySelector(".favoriteFood");
+
+userNameInput = document.querySelector("#characterName");
+ageInput = document.querySelector("#ageInput");
+updateAgeButton = document.querySelector("#updateAgeButton");
+updateNameButton = document.querySelector("#updateNameButton");
