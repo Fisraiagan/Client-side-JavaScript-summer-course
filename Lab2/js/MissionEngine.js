877200402b9fa468e223789d5dc23607cefcaee3
Fisraiagan <Francis-hagan@live.ca>
2026-05-27 14:03:55 -0400

    first if statment created with a message output

--- Lab2/js/MissionEngine.js
+++ Lab2/js/MissionEngine.js
@@ -5,3 +5,9 @@ let ammo = 8;
 let missionProgress = 40;
 let shield = true;
 let enemyNearby = true;
+
+
+	
+if(health < 30 && enemyNearby === true){
+    msg.textContent = " CRITICAL ALERT: Immediate evacuation required!";
+}
