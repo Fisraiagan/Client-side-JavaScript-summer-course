8b52571ae5f41064460161fb5a660bf3a6901b5b
Fisraiagan <Francis-hagan@live.ca>
2026-06-03 12:33:16 -0400

    Edited

--- lab3/sphero.js
+++ lab3/sphero.js
@@ -1,15 +1,16 @@
-
+let position = getLocation();
 async function startProgram() {
-    await setMainLed(getRandomColor());
-  await  traceSquare(5, 120);
+    await setMainLed(getRandomColor()); 
+    await  traceDistance(5, 120);
 
+    await speak(`my current position is ${position.x.toString()}, ${position.y.toString()}`);
     exitProgram();
 }
 
 registerEvent(EventType.onCollision, async () => {
     stopRoll();
     setMainLed({ r: 255, g: 0, b: 0 });
-
+    await speak('I crashed!', true);
     await delay(0.5);
     setMainLed({ r: 255, g: 255, b: 255 });
     setSpeed(5);
@@ -17,13 +18,16 @@ registerEvent(EventType.onCollision, async () => {
 
 });
 
+ 
 
+async function traceDistance(velocity, distance) {
 
-async function traceSquare(velocity, distance) {
-
+    await scrollMatrixText("TraceDistance", getRandomColor(), 24,false);
     await rollToDistance(30, 50, 120);
     await roll(0, 5, 60);
     await rollToDistance(80, velocity, distance);
+
+    await speak(`${distance} centimeter distance, at velocity ${velocity}`);
 
     
 }
