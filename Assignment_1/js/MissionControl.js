bdbe13031b8cb1afc3f412a27cbd162c468798ae
Fisraiagan <Francis-hagan@live.ca>
2026-06-07 10:53:55 -0400

    initalize Dom selector variables and created the markup with some Css

---
+++ Assignment_1/js/MissionControl.js
@@ -0,0 +1,17 @@
+//initalize Dom selector variables
+let agentTypeBtn = document.querySelector('#agentBtn');
+let locationBtn = document.querySelector('#locationBtn');
+let weaponLoadoutBtn = document.querySelector('#weaponLoadoutBtn');
+let objectiveBtn = document.querySelector('#objectiveBtn');
+let riskLevelBtn = document.querySelector('#riskBtn');
+let generateBtn = document.querySelector('#generateBtn');
+let resetBtn = document.querySelector('#resetBtn');
+let agentTypeOutput = document.querySelector('#agentTypeOutput');
+let locationOutput = document.querySelector('#locationOutput');
+let weaponLoadoutOutput = document.querySelector('#weaponLoadoutOutput');
+let objectiveOutput = document.querySelector('#objectiveOutput');
+let riskLevelOutput = document.querySelector('#riskLevelOutput');
+let missionOutput = document.querySelector('#missionOutput');
+
+
+
